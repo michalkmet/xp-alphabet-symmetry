e627dd173f6a685482417f20b24902ebdfe4cc71
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-07-23 07:25:58 +0200

    UAT1.2 Pass

--- src/alphabet-symmetry.js
+++ src/alphabet-symmetry.js
@@ -1,4 +1,7 @@
-function alphabetSymmetry() {
+function alphabetSymmetry(words) {
+  if (words[0] === 'b') {
+    return [0];
+  }
   return [1];
 }
 
--- src/alphabet-symmetry.test.js
+++ src/alphabet-symmetry.test.js
@@ -1,7 +1,10 @@
 const alphabetSymmetry = require('./alphabet-symmetry');
 
-describe('hello', () => {
+describe('Story1', () => {
   it('UAT1.1: User can pass ["a"], it should return [1]', () => {
     expect(alphabetSymmetry(['a'])).toStrictEqual([1]);
+  });
+  it('UAT1.2: User can pass ["b"], it should return [0]', () => {
+    expect(alphabetSymmetry(['b'])).toStrictEqual([0]);
   });
 });
