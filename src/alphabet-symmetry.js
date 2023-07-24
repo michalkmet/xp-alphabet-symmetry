f2bb82c0308033f035c1d18c084870410256184b
Michal-Kmet1 <Michal.Kmet1@ibm.com>
2023-07-24 08:38:14 +0200

    Refactor

--- src/alphabet-symmetry.js
+++ src/alphabet-symmetry.js
@@ -2,9 +2,7 @@ function alphabetSymmetry(words) {
   const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
   let counter = 0;
   [...words[0]].map((char, index) => {
-    // console.log('char: ', char);
     alphabet.map((alphabetLetter, alphabetIndex) => {
-      // console.log('alphabetLetter: ', alphabetLetter);
       if (char.toLowerCase() === alphabetLetter.toLowerCase() && index === alphabetIndex) {
         counter++;
       }
